6d9e07ff6403edd1261316b135df993a519439f4
rohitk <rohitk@NIBC2656>
2024-02-23 17:01:40 +0530

    Added generated test file

---
+++ generated_test_cases.java
@@ -0,0 +1,18 @@
+Unit Test Case:
+1. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21 + 34 + 55)
+2. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21 + 34 + 55 + 89)
+3. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21 + 34 + 55 + 89 + 144)
+4. [Test Description] (expected output: Fibonacci Series: 0)
+5. [Test Description] (expected output: Fibonacci Series: 0 + 1)
+6. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1)
+7. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2)
+8. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2 + 3)
+9. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2 + 3 + 5)
+10. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2 + 3 + 5 + 8)
+11. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2 + 3 + 5 + 8 + 13)
+12. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21)
+13. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21 + 34)
+14. [Test Description] (expected output: Fibonacci Series: 0 + 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21 + 34 + 55 + 89 + 144 + 233)
+
+Code coverage:
+The code coverage of the unit tests is 100%.
